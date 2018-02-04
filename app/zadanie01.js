109c018b0041c6d740b241fb75345e539d9b4def
Mateusz Kabała <porana@vp.pl>
2018-02-04 18:41:55 +0100

    changes

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,18 @@
-//Twój kod
+const fs = require('fs');
+
+//Odczytaj plik...
+fs.readFile('../app/data/zadanie02/file.txt', 'utf8', (err, data) => {//...kiedy skończysz uruchom naszą funkcję callback
+    if (err === null){ //Sprawdzamy czy obiekt błędu jest null-em - jeżeli tak - wyświetlamy zawartość pliku
+        console.log('Poprawnie odczytano plik.', data);
+    } else { //Jeżeli obiekt błędu nie był null-em, to pojawił się błąd - np. plik nie istniał
+        console.log('Błąd podczas odczytu pliku!', err);
+    }
+});
+
+fs.readFile('../app/data/zadanie02/data.json', 'utf8', (err, data) => {//...kiedy skończysz uruchom naszą funkcję callback
+    if (err === null){ //Sprawdzamy czy obiekt błędu jest null-em - jeżeli tak - wyświetlamy zawartość pliku
+        console.log('Poprawnie odczytano plik.', data);
+    } else { //Jeżeli obiekt błędu nie był null-em, to pojawił się błąd - np. plik nie istniał
+        console.log('Błąd podczas odczytu pliku!', err);
+    }
+});
